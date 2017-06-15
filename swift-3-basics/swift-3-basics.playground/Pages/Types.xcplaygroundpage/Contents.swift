cb605e2d0feeb34970f5682084c210e412e01033
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-06-15 16:49:06 -0500

    finishes first series of videos

---
+++ swift-3-basics/swift-3-basics.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//Types
+
+//String Literal
+let country = "United States of America"
+let state = "Minnesota"
+let city = "Minneapolis"
+let street = "Chicago Ave. S."
+let buildingNumber = 4500
+
+//Concatenation
+let address = country + " " + state + " " + city
+
+//String Interpolation
+let interpolatedAddress = "\(country), \(state), \(city)"
+let interpolatedStreetAddress = "\(buildingNumber) \(street)"
+
+//Using String Interpolation we can evaluate more than just strings
+
---
+++ swift-3-basics/swift-3-basics.playground/Pages/Variables and Constants.xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+//Variables
+
+var str = "Hello, playground"
+str = "Hello, world!"
+
+var number = 10
+number
+print(number)
+
+//Constants
+let language = "Swift"
+
+//Naming Conventions
+/*
+ Spaces are not allowed
+ let programming language = "Objective-C"
+*/
+
+//Use camelCase for multiple words
+
+let programmingLanguage = "Objective-C"
+
+//Disallowed characters
+//First char cannot be a number
+//No spaces allowed
+//No symbols special characters or math operators allowed
+
+//Always start by declaring a constant first. 
+
+//End first video
+
+
