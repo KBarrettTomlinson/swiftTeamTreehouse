62d01820edcffc090d6c113efdbf10af0575ed99
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-06-16 19:51:42 -0500

    team treehouse swit

---
+++ swift-3-basics/swift-3-basics.playground/Pages/Operators.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+/*
+ --------------------------
+ Mathematical Operators
+ --------------------------
+ */
+
+//Unary
+//Binary
+//Ternary
+
+//Binary
+//Operand
+//Expressions
+
+//Infix operators
+
+//Binary
+let height:Double = 45 //in ft
+let width:Double = 23 //in ft
+
+let area: Double = height * width //Area in sq ft
+
+//1 sq meter = 1 sq foot / 10.764
+
+let areaInMeters: Double = area/10.764
--- swift-3-basics/swift-3-basics.playground/Pages/Types.xcplaygroundpage/Contents.swift
+++ swift-3-basics/swift-3-basics.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -16,3 +16,54 @@ let interpolatedStreetAddress = "\(buildingNumber) \(street)"
 
 //Using String Interpolation we can evaluate more than just strings
 
+//Integers
+let favoriteProgrammingLanguage = "Swift"
+let year = 2014 //Int
+
+/*
+ --------------------------
+ Floating Point Numbers
+ --------------------------
+ */
+
+var version = 3.0 //Double
+
+//Type Double - at least 15 decimal digits
+//Type Float - as little as 6 deciaml digits
+
+//Use Double unless you specifically need float
+
+/*
+ --------------------------
+ Boolean
+ --------------------------
+ */
+
+let isAwesome = true
+
+//True = 1
+//False = 0
+
+
+/*
+ --------------------------
+ Type Safety
+ --------------------------
+ */
+
+//Swift is a static type language
+//The compiler is typechecking
+//We can explicitly state the type
+
+let bestPlayer: String = "Michael Jordan"
+let averagePointsPerGame: Double = 30.1
+let yearOfRetirement: Int = 2003
+let hallOfFame: Bool = true
+
+//Explicit typing allows for more readable code
+
+
+
+
+
+
